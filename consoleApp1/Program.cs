3dec976473dc551af0c47a5c738c8e4d9cacbcc8
routhuganesh <routhuganesh35@gmail.com>
2022-12-04 18:55:45 +0530

    Add project files.

---
+++ consoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        if (n % 2 == 0)
+        {
+            Console.WriteLine("Even number");
+        }
+        else
+        {
+            Console.WriteLine("Odd number");
+        }
+    }
+
+}
